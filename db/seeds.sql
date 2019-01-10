3ae7c6da15562d9c944e7cd971305a2db8e10735
dstark88 <starkwalker88@gmail.com>
2019-01-09 18:27:25 -0700

    finished with tutoring

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Colorado Burger');
+INSERT INTO burgers (burger_name, devour) VALUES ('Elk Burger', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Hawaiian Burger', true);
